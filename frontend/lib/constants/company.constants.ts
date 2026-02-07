cc4c3473f209df7da0ed4658d9f163a05b5dffb8
Gabriel Linck <gabriel.jobc13@gmail.com>
2026-02-07 00:09:17 -0300

    chore(company_id): update company_id on constants

--- frontend/lib/constants/company.constants.ts
+++ frontend/lib/constants/company.constants.ts
@@ -12,4 +12,4 @@
  * NÃO edite manualmente.
  */
 
-export const DEFAULT_COMPANY_ID = '9d8fb179-ede4-4ea5-98d2-6db222ba6f66';
+export const DEFAULT_COMPANY_ID = 'a4771684-cd63-4ecd-8771-545ddb937278';
